24208cf3aa12592a08c9fde978e81d0513c1dbe0
Tommy Thomas <tommyt@uchicago.edu>
2014-02-11 16:10:44 -0600

    adding correct permissions lab

--- DangerousApp/gen/course/labs/dangerousapp/Manifest.java
+++ DangerousApp/gen/course/labs/dangerousapp/Manifest.java
@@ -9,6 +9,13 @@ package course.labs.dangerousapp;
 
 public final class Manifest {
     public static final class permission {
+        /** 
+          TODO - Using a permission element,
+          define a custom permission with name
+    		  "course.labs.permissions.DANGEROUS_ACTIVITY_PERM" 
+          and "dangerous" protection level.
+    
+         */
         public static final String DANGEROUS_ACTIVITY_PERM="course.labs.permissions.DANGEROUS_ACTIVITY_PERM";
     }
 }
