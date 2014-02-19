1277f4879597d80d9e2bfc201a63a4ec97621ceb
tommy-thomas <tommyt@ucicago.edu>
2014-02-18 23:32:00 -0600

    going on with uilab

--- UILabs/src/course/labs/todomanager/ToDoListAdapter.java
+++ UILabs/src/course/labs/todomanager/ToDoListAdapter.java
@@ -86,11 +86,14 @@ public class ToDoListAdapter extends BaseAdapter {
 
 
 		//TODO - Get the current ToDoItem
-		final ToDoItem toDoItem = null;
+		final ToDoItem toDoItem = (ToDoItem)getItem(position);
 
 		//TODO - Inflate the View for this ToDoItem
 		// from todo_item.xml.
-		RelativeLayout itemLayout = null;
+		 LayoutInflater inflater = (LayoutInflater)mContext
+			        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		RelativeLayout itemLayout = (RelativeLayout)inflater.inflate(R.layout.todo_item, parent , false);
+		
 		
 		//TODO - Fill in specific ToDoItem data
 		// Remember that the data that goes in this View
@@ -98,13 +101,11 @@ public class ToDoListAdapter extends BaseAdapter {
 		// in the layout file 
 
 		//TODO - Display Title in TextView
-
-		final TextView titleView = null;
+		final TextView titleView = (TextView)itemLayout.findViewById(R.id.titleView);
+		titleView.setText(toDoItem.getTitle());
 		
 		// TODO - Set up Status CheckBox
-	
-		final CheckBox statusView = null;
-		
+		final CheckBox statusView = (CheckBox)itemLayout.findViewById(R.id.statusCheckBox);
 		
 		statusView.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			@Override
@@ -114,22 +115,19 @@ public class ToDoListAdapter extends BaseAdapter {
 				
 				// TODO - Set up and implement an OnCheckedChangeListener, which 
 				// is called when the user toggles the status checkbox
-
-
-			
+				
 			}
 		});
 
 		//TODO - Display Priority in a TextView
-
-		final TextView priorityView = null;
+		final TextView priorityView = (TextView)itemLayout.findViewById(R.id.priorityView);
+		priorityView.setText(toDoItem.getPriority().toString());
 
 		
 		// TODO - Display Time and Date. 
 		// Hint - use ToDoItem.FORMAT.format(toDoItem.getDate()) to get date and time String
-
-		final TextView dateView = null;
-				
+		final TextView dateView = (TextView)itemLayout.findViewById(R.id.dateView);
+		dateView.setText(ToDoItem.FORMAT.format(toDoItem.getDate()));	
 
 		// Return the View you just created
 		return itemLayout;
